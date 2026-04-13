69bd980a1b30b9d3dd47766d5c984ee3d0e3980a
ccl <chenlam.chan@gmail.com>
2026-04-13 16:16:53 +0800

    fix: Solved the issue of missing env_file parameter in yml file - scheduler is not able to find the table

--- infrastructures/postgres/postgres-init.sh
+++ infrastructures/postgres/postgres-init.sh
@@ -11,12 +11,16 @@ export PGPASSWORD="$SUPER_PASS"
 psql -h postgres -U "$SUPER_USER" -d postgres \
   -v targetdb="$TARGET_DB" \
   -v airflow_pass="$AIRFLOW_PASS" <<-'EOSQL'
+    -- Create airflow role if not exists
     SELECT format('CREATE ROLE airflow WITH LOGIN PASSWORD %L', :'airflow_pass')
     WHERE NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'airflow')\gexec
 
+    -- Create DB if not exists (may already exist from POSTGRES_DB auto-creation)
     SELECT format('CREATE DATABASE %I OWNER airflow', :'targetdb')
     WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = :'targetdb')\gexec
 
+    -- Always ensure correct ownership & privileges regardless of who created it
+    SELECT format('ALTER DATABASE %I OWNER TO airflow', :'targetdb')\gexec
     SELECT format('GRANT ALL PRIVILEGES ON DATABASE %I TO airflow', :'targetdb')\gexec
 EOSQL
 
