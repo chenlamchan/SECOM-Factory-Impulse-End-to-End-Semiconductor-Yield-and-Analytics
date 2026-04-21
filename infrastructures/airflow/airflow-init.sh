504a8015a882881b63cca82832c19e2a414c5ae7
ccl <chenlam.chan@gmail.com>
2026-04-21 13:42:52 +0800

    fix secom bronze layer code to the sensor missing rate.

--- infrastructures/airflow/airflow-init.sh
+++ infrastructures/airflow/airflow-init.sh
@@ -21,18 +21,24 @@ fi
 ADMIN_USER=${AIRFLOW_ADMIN_USER}
 ADMIN_EMAIL=${AIRFLOW_ADMIN_EMAIL:-admin@example.com}
 
+set +e
+
 # Create user ONLY if it doesn't exist
-if ! airflow users list | grep -q "$ADMIN_USER"; then
-    airflow users create \
-      --role Admin \
-      --username "$ADMIN_USER" \
-      --password "$ADMIN_PASSWORD" \
-      --email "$ADMIN_EMAIL" \
-      --firstname Admin \
-      --lastname User
+airflow users create \
+    --role Admin \
+    --username "$ADMIN_USER" \
+    --password "$ADMIN_PASSWORD" \
+    --email "$ADMIN_EMAIL" \
+    --firstname Admin \
+    --lastname User
+
+if [ $? -eq 0 ]; then
+    echo "Admin user created successfully."
 else
-    echo "Admin user already exists, skipping creation."
+    echo "Admin user already exists or creation bypassed."
 fi
+
+set -e
 
 create_spark_conn() {
   airflow connections add 'spark_default' \
